3ba243d704733c706a65927337296f5135ba1691
zhongxm <zhongxiaoming@52tt.com>
2022-07-14 20:20:07 +0800

    Initial Commit

---
+++ MyFrameworkA/MyFrameworkA.h
@@ -0,0 +1,18 @@
+//
+//  MyFrameworkA.h
+//  MyFrameworkA
+//
+//  Created by samingzhong on 2022/7/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFrameworkA.
+FOUNDATION_EXPORT double MyFrameworkAVersionNumber;
+
+//! Project version string for MyFrameworkA.
+FOUNDATION_EXPORT const unsigned char MyFrameworkAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFrameworkA/PublicHeader.h>
+
+
